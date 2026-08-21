d244d159e1f198b55e898d19230ddb8b0821e4fb
Evin M Padhi <evinmpadhi@Evins-Mac-Studio.local>
2026-08-20 23:10:13 -0700

    fix: support GenMap bg output extension

--- tests/test_transqtl_mappability_container.sh
+++ tests/test_transqtl_mappability_container.sh
@@ -30,7 +30,7 @@ docker run --rm \
     } > "$tmpdir/reference.fa"
     genmap index -F "$tmpdir/reference.fa" -I "$tmpdir/index"
     genmap map -K 20 -E 2 -I "$tmpdir/index" -O "$tmpdir/mappability" -t -w -bg
-    bedgraph="$(find "$tmpdir" -type f -name "*.bedGraph" -print -quit)"
+    bedgraph="$(find "$tmpdir" -type f \( -name "*.bedGraph" -o -name "*.bg" \) -print -quit)"
     test -n "$bedgraph"
     test -s "$bedgraph"
     awk '\''BEGIN { OFS = "\t" } $4 < 1 { print $1, $2, $3 }'\'' "$bedgraph" \
